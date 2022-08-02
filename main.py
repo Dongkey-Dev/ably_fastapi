38b52837d328cb3804b0bdb8bb5964b1266d212c
Dongkey-Dev <dongkeydev@gmail.com>
2022-08-02 18:43:33 +0900

    fix : Solve conflict

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import uvicorn
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    load_test.loadtest.run()
-
 @app.get("/")
 def read_root():
     return {"Hello": "world"}
-
-uvicorn.run(app)
