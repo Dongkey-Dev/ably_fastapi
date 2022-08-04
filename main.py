42b765aec77f83a5733150165d272c3d412449a5
Dongkey-Dev <dongkeydev@gmail.com>
2022-08-05 02:26:41 +0900

    add : Main requirements

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from typing import Union
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "world"}
+@app.post("/auth/regist_user")
+def regist_user():
+    pass
+
+@app.post("/auth/login_user")
+def login_user():
+    pass
+
+@app.post("/auth/reset_pswd")
+def reset_pswd():
+    pass
+
+@app.post("/auth/check_my_info")
+def check_my_info():
+    pass
