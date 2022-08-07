31e9fe56780834b9250cf0b2f99299ef1293d275
Dongkey-Dev <dongkeydev@gmail.com>
2022-08-08 00:51:11 +0900

    Crunch_1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
 from routes import auth, inquire
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.middleware.cors import CORSMiddleware
 
 from app.db.dbconn import db
 
@@ -12,11 +10,7 @@ def create_app():
     app = FastAPI()
 
     app.include_router(auth.router, tags=["Authentication"], prefix="/api")
-    app.include_router(inquire.router, tags=["service"], prefix="/api")
+    app.include_router(inquire.router, tags=["Service"], prefix="/api")
     return app
 
 app = create_app()
-
-if __name__=="__main__":
-   uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) 
-    
