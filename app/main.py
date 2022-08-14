f87448b4ece5dcc16881d8794b30aa18c7376daf
Dongkey-Dev <dongkeydev@gmail.com>
2022-08-14 18:26:39 +0900

    modify : Modify port

--- app/main.py
+++ app/main.py
@@ -27,5 +27,5 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000,
+    uvicorn.run("main:app", host="0.0.0.0", port=8081,
                 reload=False, log_level="debug", debug=True)
